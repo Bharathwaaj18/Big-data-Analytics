dbd3870cb87b6b2d975fcb188e961cd584435bbd
Bharathwaaj18 <122618653+Bharathwaaj18@users.noreply.github.com>
2024-06-13 21:00:07 +0530

    Create wc runner.java

---
+++ wc runner.java
@@ -0,0 +1,22 @@
+package com.mapreduce.java;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred. JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred. TextOutputFormat;
+public class WC_Runner
+public static void main(String[] args) throws IOExceptior JobConf conf new JobConf(WC_Runner.class);
+conf.setJobName("WordCount"); conf.setOutputKeyClass(Text.class);
+conf.setOutputValueClass(IntWritable.class);
+conf.setMapperClass (WC_Mapper.class);
+conf.setCombinerClass(WC_Reducer.class);
+conf.setReducerClass (WC Reducer.class);
+conf.setInputFormat(TextInputFormat.class);
+conf.setOutputFormat(TextOutputFormat.class);
+FilelnputFormat.setInputPaths(conf.new Path(args[0]));
+FileOutputFormat.setOutputPath(conf.new Path(args[1])): JobClient.runJob(conf);
